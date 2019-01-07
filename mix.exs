067488cef4992ca0e78873de8882f94eaa1992ba
Roger Souza <rogersec42@gmail.com>
2019-01-07 19:29:32 -0300

    dependencies: add mox, a library to test our calls to external API's.

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Dienekes.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:mox, "~> 0.3", only: :test}
     ]
   end
 
