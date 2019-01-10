8d4825badd69b10cc2ce35b2d4b460eb5b59b3d3
Roger Souza <rogersec42@gmail.com>
2019-01-10 11:22:07 -0300

    http client: Some refactoring to make code readable.

--- lib/dienekes/numbers/api_client.ex
+++ lib/dienekes/numbers/api_client.ex
@@ -1,3 +1,3 @@
 defmodule Dienekes.Numbers.APIClient do
-  @callback get_numbers_from(String.t()) :: {:ok, list()} | {:error, String.t()}
+  @callback get_numbers_from(integer()) :: {:ok, list()} | {:error, String.t()}
 end
--- lib/dienekes/numbers/http_client.ex
+++ lib/dienekes/numbers/http_client.ex
@@ -5,18 +5,26 @@ defmodule Dienekes.Numbers.HTTPClient do
 
   @api_endpoint Application.get_env(:dienekes, :api_endpoint)
 
-  @impl true
-  @spec get_numbers_from(integer()) :: {:ok, list()} | {:error, String.t()}
-  def get_numbers_from(page_index) when page_index > 0 and is_integer(page_index) do
-    endpoint = @api_endpoint <> "?page=#{page_index}"
+  @simulated_internal_error %{body: "{\"error\":\"Simulated internal error\"}"}
+  @empty_list %{"numbers" => []}
 
-    with {:ok, %{status_code: 200, body: body}} <- HTTPoison.get(endpoint, [], [timeout: 50_000, recv_timeout: 50_000]),
+  @impl true
+  def get_numbers_from(page_index) when page_index > 0 do
+    endpoint = build_url(page_index)
+
+    get_request = fn ->
+      HTTPoison.get(endpoint, [], timeout: 50_000, recv_timeout: 50_000)
+    end
+
+    with {:ok, %{status_code: 200, body: body}} <- get_request.(),
          {:ok, %{"numbers" => numbers}} when numbers != [] <- Jason.decode(body) do
       {:ok, numbers}
     else
-      {:ok, %{"numbers" => []}} -> {:error, "empty list"}
-      {:ok, %{body: "{\"error\":\"Simulated internal error\"}"}} -> {:error, "internal error"}
+      {:ok, @empty_list} -> {:error, "empty list"}
+      {:ok, @simulated_internal_error} -> {:error, "internal error"}
       {:error, reason} -> {:error, reason}
     end
   end
+
+  defp build_url(page_index), do: @api_endpoint <> "?page=#{page_index}"
 end
